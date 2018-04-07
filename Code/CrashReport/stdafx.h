69285555febff5a13f8376a84b667289a102f494
havenow <715667426@qq.com>
2018-04-07 10:00:09 +0800

    异常处理机制

--- Code/CrashReport/stdafx.h
+++ Code/CrashReport/stdafx.h
@@ -10,12 +10,31 @@
 #define WIN32_LEAN_AND_MEAN             // 从 Windows 头中排除极少使用的资料
 // Windows 头文件: 
 #include <windows.h>
+#include <WinSock2.h>
+#pragma comment(lib, "ws2_32.lib")
 
 // C 运行时头文件
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
 #include <tchar.h>
+
+#include <string>
+#include <vector>
+using namespace std;
+
+#include <AppHelper.h>
+#include <PublicLib/XYLog.h>
+#include <PublicLib/StrHelper.h>
+#include <PublicLib/Common.h>
+#include <PublicLib/json/json-forwards.h>
+#include <PublicLib/json/json.h>
+#include <PublicLib/md5.h>
+#include <PublicLib/HttpClient.h>
+#include <PublicLib/Markup.h>
+#include <PublicLib/System.h>
+#include <PublicLib/FileHelper.h>
+#include <atlfile.h>
 
 
 // TODO:  在此处引用程序需要的其他头文件
